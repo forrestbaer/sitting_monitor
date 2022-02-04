d3559e87a7b76b59f818b8702b92aa7d4fb45bd0
Forrest Baer <forrest@forrestbaer.com>
2022-02-04 16:44:27 -0600

    logger outputting correct hrs per day

--- log.js
+++ log.js
@@ -1,7 +1,10 @@
+import fs from 'fs'
+import _  from 'lodash'
+import chalk from 'chalk'
+
 (() => {
-  const fs = require('fs')
-  const _ = require('lodash')
   const width = 88
+  const log = console.log 
 
   fs.readFile('../../log/sitmon.log', 'utf8' , (err, data) => {
     if (err) {
@@ -17,7 +20,7 @@
       let t = new Date(`${i[0]} ${i[1]}`).toLocaleTimeString()
       let ts = t.split(':')
       let tss = ts.slice(0, ts.length-1)
-      let mins = (parseInt(tss[0])*60)+parseInt(tss[1])
+      let mins = (parseFloat(tss[0])*60)+parseFloat(tss[1])
       let percent = parseFloat(mins / 1440)
 
       return {
@@ -31,12 +34,27 @@
 
     d = _.groupBy(d, 'date')
 
-    Object.keys(d).forEach(k => {
-      console.log(k)
-      d[k].forEach(j => {
-        let charpos = parseFloat(j.percent * width).toFixed(2)
-        console.log(`${charpos} -- ${j.position}`)
+    Object.keys(d).forEach(day => {
+      console.log(chalk.gray('-------------------------'))
+      console.log(' ' + chalk.whiteBright.bgGray(day))
+      console.log(chalk.gray('-------------------------'))
+
+      let tm = 0
+      let ct = 0
+      let lp = ''
+
+      d[day].forEach(j => {
+        if (j.position === 'sat' && (lp === 'stood' || lp === '')) {
+          lp = 'sat'
+          ct = j.minutes
+        }
+        if (j.position === 'stood' && (lp === 'sat' || lp === '')) {
+          lp = 'stood'
+          tm = tm + (j.minutes - ct)
+        }
       })
+
+      console.log(`sat for ${Math.floor(tm / 60)}hrs and ${tm % 60} minutes\n`)
     })
   })
 })()
